c46d293334fd89c28969cc77725472f8bfc76e57
Blinov Danil <59275466+blinovarc@users.noreply.github.com>
2020-10-12 01:02:14 +0300

    fix(CI): Обновил swift-syntax

--- Package.swift
+++ Package.swift
@@ -12,7 +12,7 @@ let package = Package(
             targets: ["variable-injector"]),        
     ],
     dependencies: [
-        .package(name: "SwiftSyntax", url: "https://github.com/apple/swift-syntax.git", .exact("0.50300.0")),
+        .package(name: "SwiftSyntax", url: "https://github.com/apple/swift-syntax.git", .exact("swift-DEVELOPMENT-SNAPSHOT-2020-10-07-a")),
         .package(name: "swift-argument-parser", url: "https://github.com/apple/swift-argument-parser.git", .exact("0.3.1"))
     ],
     targets: [
